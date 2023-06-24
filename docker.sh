6663003bb5f85726225511986e2932b161c264c2
ykdojo <yksu@csdojo.io>
2023-06-24 03:42:15 +0000

    Update docker.sh to add a check for existing image before building
    - Added a check to see if the Docker image already exists before building it. This is to optimize the script by avoiding unnecessary image builds, which can be time-consuming, and directly using the existing image if it's already available.

--- docker.sh
+++ docker.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Build the Docker image
-docker build -t kaguya .
+# Name of the image
+IMAGE_NAME="kaguya"
+
+# Check if the image already exists
+if [ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]; then
+  echo "Image does not exist. Building..."
+  # Build the Docker image
+  docker build -t $IMAGE_NAME .
+else
+  echo "Image exists. Starting container..."
+fi
 
 # Run the Docker container with the --rm flag (automatically remove container on exit)
-docker run --shm-size=2g --rm -p 3000:3000 -v $(pwd):/kaguya -v /kaguya/node_modules kaguya
+docker run --shm-size=2g --rm -p 3000:3000 -v $(pwd):/kaguya -v /kaguya/node_modules $IMAGE_NAME
