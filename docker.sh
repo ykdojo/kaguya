5d71d177ba8974a23738a7e3b191e0bdf663f042
ykdojo <yksu@csdojo.io>
2023-06-24 08:46:02 +0000

    Update docker.sh to rebuild image only if Dockerfile is modified
    - Updated docker.sh to use the modification timestamp of Dockerfile to determine if the image needs to be rebuilt.
    - Added .last_build_timestamp to .gitignore with a comment explaining why it should be ignored.

--- docker.sh
+++ docker.sh
@@ -3,13 +3,21 @@
 # Name of the image
 IMAGE_NAME="kaguya"
 
-# Check if the image already exists
-if [ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]; then
-  echo "Image does not exist. Building..."
+# File to store the timestamp of the last build
+TIMESTAMP_FILE=".last_build_timestamp"
+
+# Get the modification timestamp of Dockerfile
+MOD_TIMESTAMP=$(date -r Dockerfile +%s)
+
+# Check if the image already exists and if the timestamp has changed
+if [ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ] || [ ! -f "$TIMESTAMP_FILE" ] || [ "$MOD_TIMESTAMP" != "$(cat $TIMESTAMP_FILE)" ]; then
+  echo "Building the image..."
   # Build the Docker image
   docker build -t $IMAGE_NAME .
+  # Store the modification timestamp of Dockerfile
+  echo "$MOD_TIMESTAMP" > $TIMESTAMP_FILE
 else
-  echo "Image exists. Starting container..."
+  echo "Image exists and is up to date. Starting container..."
 fi
 
 # Run the Docker container with the --rm flag (automatically remove container on exit)
